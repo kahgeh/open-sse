cce76e322c292916d16dd7c98267d35340381cdc
kahgeh <kahgeh@hotmail.com>
2021-07-23 22:31:34 +1000

    minor fixes

--- webclient/App.tsx
+++ webclient/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 const sseClientId = Date.now();
 function App() {
